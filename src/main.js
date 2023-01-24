25698be7bd9ef9f7cc02bb6754bb34b4c5a5d4ef
Richard Gao <richardrxgao@gmail.com>
2023-01-24 11:50:55 +1100

    element ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/assets/scss/reset.scss";
+import "@/assets/font/iconfont.css";
+import "@/assets/scss/common.scss"
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
